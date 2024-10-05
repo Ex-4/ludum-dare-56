0a90eeb38d00395deff232f386a625fcbe2527d0
Ex-4 <146730152+Ex-4@users.noreply.github.com>
2024-10-05 11:31:21 +0100

    setup and rat player

---
+++ Scripts/Player.gd
@@ -0,0 +1,43 @@
+extends CharacterBody2D
+
+#signal health_depleted
+
+var last_direction
+var health = 5.0
+var direction
+
+func _ready():
+	pass
+	#$HealthBar.set_max(health)
+
+func _process(delta):
+	pass
+	#if last_direction:
+		#$Sprite2D.rotation = last_direction
+
+func _physics_process(delta):
+	direction = Input.get_vector("move_left", "move_right", 
+	"move_up", "move_down") #wasd
+	
+	#	rotating the player sprite when moving
+	if Input.is_action_just_pressed("move_left"):
+		$Sprite2D.rotation_degrees = 180
+	elif Input.is_action_just_pressed("move_right"):
+		$Sprite2D.rotation_degrees = 0
+	elif Input.is_action_just_pressed("move_down"):
+		$Sprite2D.rotation_degrees = 90		
+	elif Input.is_action_just_pressed("move_up"):
+		$Sprite2D.rotation_degrees = 270
+			
+	velocity = direction * 400		
+	move_and_slide()
+
+func _input(event):
+
+	pass
+
+func take_damage():
+	health-=1
+	$HealthBar.value = health
+	if health <= 0:
+		pass
